5ad833ec2f5764425fe17235287325f5b3b224b4
Vaibhavi patel <patelvaibhavi2412@gmail.com>
2024-09-22 18:25:08 +0530

    new

---
+++ 1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    val a:Int = 5
+    println("Integer value = $a")
+    val b:Float = 30.00F
+    println("Floating value = $b")
+    val c:Char = 'v'
+    println("Character value = $c")
+    val d:String = "vaibhavi"
+    println("String value = $d")
+    val e:Double = 30.56
+    println("Double value = $e")
+    val f:Long = 15000000000L
+    println("Long value = $f")
+    val g:Short = 5000
+    println("Short value = $g")
+    val h:Boolean = false
+    println("Boolean value = $h")
+    val i:Byte = 127
+    println("Byte value = $i")
+}
